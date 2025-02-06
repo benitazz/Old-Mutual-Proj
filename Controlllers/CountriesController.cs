701e85915d11a7a3c5b94e6a366fa1cd5633a95f
ben.baloyi <benitazz@gmail.com>
2025-02-06 22:56:13 +0200

    Put back the deleted code

---
+++ Controlllers/CountriesController.cs
@@ -0,0 +1,52 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/countries")]
+public class CountriesController : ControllerBase
+{
+    private readonly ICountryService _countryService;
+    private readonly IMapper _mapper;
+
+    public CountriesController(ICountryService countryService, IMapper mapper)
+    {
+        _countryService = countryService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCountries()
+    {
+        try
+        {
+            var countries = await _countryService.GetAllCountriesAsync();
+
+            var countryDtos = this._mapper.Map<List<CountryDto>>(countries);
+
+            return Ok(countryDtos);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{name}")]
+    public async Task<IActionResult> GetCountryDetails(string name)
+    {
+        try
+        {
+            var countryDetail = await _countryService.GetCountryDetailsAsync(name);
+            if (countryDetail == null)
+                return NotFound();
+
+            var countryDetailsDto = this._mapper.Map<CountryDetailsDto>(countryDetail);
+
+            return Ok(countryDetailsDto);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
